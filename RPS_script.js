90a280ff56b658a1b3bd266facb8d753d9e7b342
coderecruit <tiagosilva.workemail@gmail.com>
2021-04-02 20:00:25 +0000

    no progress so far

---
+++ RPS_script.js
@@ -0,0 +1,38 @@
+
+// 1. Make computerPlay return Rock, Paper or Scisor 
+
+let computerPlay = ["Rock", "Paper", "Scisor"];
+
+
+function computerSelection() {
+    return randomItem = computerPlay [Math.floor(Math.random()*computerPlay.length)];
+
+}
+
+
+// 2. Make computerPlay() random
+
+
+
+// 3. Prompt for playerSelection()
+
+
+// 4. Acept only strings
+
+
+// 5. Make it case insensitive
+
+
+// 6. Play a round vs computer 
+
+
+// 7. Display result of round
+
+
+// 8. Keep score of rounds
+
+
+/* 9. Play 5 rounds 
+    Use loops or repeat function 5 times */
+
+
