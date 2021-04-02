5b19ffd652a73093a3040c25084441084c5f310a
coderecruit <tiagosilva.workemail@gmail.com>
2021-04-02 21:29:00 +0000

    random computer play progress

--- RPS_script.js
+++ RPS_script.js
@@ -1,16 +1,34 @@
 
-// 1. Make computerPlay return Rock, Paper or Scisor 
+//
+// 1. Make a function for the computer random selection return Rock, Paper or Scisor 
+//
 
-let computerPlay = ["Rock", "Paper", "Scisor"];
 
-
-function computerSelection() {
-    return randomItem = computerPlay [Math.floor(Math.random()*computerPlay.length)];
-
+function computerPlay () {
+    let computerOptions = ["Rock", "Paper", "Scisor"];
+    let random = computerOptions[Math.floor(Math.random()*computerOptions.length)];
+    return random;
 }
 
 
+
+
+/* This returns a selection, but needs to reload the page to get a new selection. Only valid for 1 round
+let computerPlay = ["Rock", "Paper", "Scisor", ];
+var randomItem = computerPlay[Math.floor(Math.random()*computerPlay.length)];
+*/
+
+/* Drafts
+function computerSelection() {
+    return randomItem = computerPlay [Math.floor(Math.random()*computerPlay.length)];
+}
+*/
+
+
+
+//
 // 2. Make computerPlay() random
+//
 
 
 
