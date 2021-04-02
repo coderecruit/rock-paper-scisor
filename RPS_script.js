0698ed87d153a2d161764f7b77f0115066b2a426
coderecruit <tiagosilva.workemail@gmail.com>
2021-04-02 23:03:21 +0000

    working on if conditionals to only accept RPS. Not finished. It is accepting any string and always displays playerSelection to the console no matter what is the input

--- RPS_script.js
+++ RPS_script.js
@@ -1,47 +1,48 @@
 
 //
 // 1. Make a function for the computer random selection return Rock, Paper or Scisor 
-//
-
-
-function computerPlay () {
-    let computerOptions = ["Rock", "Paper", "Scisor"];
-    let random = computerOptions[Math.floor(Math.random()*computerOptions.length)];
-    return random;
-}
-
-
-
-
-/* This returns a selection, but needs to reload the page to get a new selection. Only valid for 1 round
-let computerPlay = ["Rock", "Paper", "Scisor", ];
-var randomItem = computerPlay[Math.floor(Math.random()*computerPlay.length)];
-*/
-
-/* Drafts
-function computerSelection() {
-    return randomItem = computerPlay [Math.floor(Math.random()*computerPlay.length)];
-}
-*/
-
-
-
-//
 // 2. Make computerPlay() random
 //
 
+function computerPlay () {
+    let computerOptions = ["Rock", "Paper", "Scissor"];
+    let random = computerOptions[Math.floor(Math.random()*computerOptions.length)];
+    return random;
+}
+ 
+console.log(computerPlay());
 
-
+//
 // 3. Prompt for playerSelection()
+//
+
+
+
+let playerSelection = prompt("Choose: Rock, Paper or Scissor", "");
+
+if (playerSelection === "Paper" || "Rock" || "Scissor") {
+    console.log(playerSelection);
+} else {
+    alert ('Invalid input! Reload the page and try again');
+}
+
 
 
 // 4. Acept only strings
+
+
 
 
 // 5. Make it case insensitive
 
 
 // 6. Play a round vs computer 
+
+
+
+// 6.1 Decide who wins each round
+
+
 
 
 // 7. Display result of round
